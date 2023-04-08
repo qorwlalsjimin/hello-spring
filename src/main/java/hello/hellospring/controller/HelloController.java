63410d2a585c1d49df83097f5df5b0aef558a4ed
지민 백 <s2104@e-mirim.hs.kr>
2023-04-08 11:18:13 +0900

    #8 주석 추가

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -14,9 +14,15 @@ public class HelloController {
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam(name = "name") String name, Model model){
+    public String helloMvc(@RequestParam(name = "name") String name,
+                           //name = "name"의 "name"으로 값(GET 방식)을 입력하게 한다
+                           //String name의 name으로 웹주소의 값을 받아와 java에서 사용하게 한다
+                           Model model){
+
         //model에 담으면 View에서 랜더링할때 쓰인다
+        //html에서 name이라는 attributeName으로 value를 불러옴
         model.addAttribute("name", name); //key, value
+
         return "hello-template";
     }
 }
