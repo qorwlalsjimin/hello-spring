ac524505ce14fd8d81e3571101c6dbed1952b1a9
지민 백 <s2104@e-mirim.hs.kr>
2023-04-08 10:59:42 +0900

    #7 정적 컨텐츠

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +11,12 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "spring!"); //model(data:hello!)
         return "hello"; // hello.html로 이동 (templates/hello.html)
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        //model에 담으면 View에서 랜더링할때 쓰인다
+        model.addAttribute("name", name); //key, value
+        return "hello-template";
     }
 }
