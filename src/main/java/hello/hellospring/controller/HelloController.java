7ce761e4b1234ce225d2acc3f6b103c8eca633ab
지민 백 <s2104@e-mirim.hs.kr>
2023-04-12 14:51:08 +0900

    #9 json 형식으로 클라이언트에 객체 내려주기

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -24,5 +25,30 @@ public class HelloController {
         model.addAttribute("name", name); //key, value
 
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http의 body에 이 데이터를 넣겠다는 의미
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name; //hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        //http://localhost:8080/hello-api?name=strp
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private  String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
