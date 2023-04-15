9c01f489a1aacbf003a2d04ddb681348523d87fb
지민 백 <s2104@e-mirim.hs.kr>
2023-04-15 11:22:39 +0900

    #16 setter 주입

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,21 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
-
+@Service
 public class MemberService {
-    @Autowired private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-//    @Autowired
-//    public MemberService(MemberRepository memberRepository){
-//        this.memberRepository = memberRepository;
-//    }
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
