2150b2792875cabc6ca59c068dce5d6b6200d23a
지민 백 <s2104@e-mirim.hs.kr>
2023-04-15 11:23:07 +0900

    #16 제일 권장되는 생성자 주입

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,12 +9,7 @@ public class MemberController {
     private MemberService memberService;
 
     @Autowired
-    public void setMemberService(MemberService memberService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
-//    @Autowired
-//    public MemberController(MemberService memberService) {
-//        this.memberService = memberService;
-//    }
 }
