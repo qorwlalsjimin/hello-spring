2867fad4232da486af634d95f67e5916a3c67e46
지민 백 <s2104@e-mirim.hs.kr>
2023-04-15 12:13:32 +0900

    #17 회원가입 기능

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name; //input 태그의 name과 매칭
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
